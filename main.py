0c6e1cab8846844d3732ae231316311852c9845a
Arkadev Banerjee <arkadev220701@gmail.com>
2021-03-13 12:07:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 import os
+import telegram
 import pyautogui as gui
 from datetime import datetime
-import telebot
-from telethon.sync import TelegramClient
-from telethon.tl.types import InputPeerUser, InputPeerChannel
-from telethon import TelegramClient
 
-# ****************************************************************
-#              INDIVIDUAL UNIQUE SECRET INFORMATION
-# ****************************************************************
-
-bot_api_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' # YOUR BOT API TOKEN
-
-api_id = 1111111 # YOUR API ID
-
-api_hash = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' # YOUR API HASH
-
-phone = '+91XXXXXXXXXX' # YOUR PHONE NUMBER
-
-SEND_TO = 'xxxxxxxxxx' # ID of the Recipient - (MUST BE PUBLIC)
+COUNTRY_CODE = '+91'
 
 # ****************************************************************
 #              CREATING SETUP FILES (FIRST TIME ONLY)
@@ -39,6 +24,49 @@ if 'send.vbs' not in f_list:
 WshShell.Run chr(34) & "send.bat" & Chr(34), 0
 Set WshShell = Nothing''')
 
+inputs = [
+    'YOUR_BOT_API_TOKEN_HERE.txt',
+    'RECEIVER_ID_HERE.txt',
+]
+
+flag = 0
+
+for it in f_list:
+    for inp in inputs:
+        if inp not in f_list:
+            flag = 1
+            with open(inp, 'w') as fp:
+                pass
+
+if flag == 1:
+    print('Setup Successful! Fill in your details in the files created and re-run the script again.')
+    print('To Set a Keybinding, Create a shortcut of the \'send.vbs\' file and set a keybinding there.')
+    print('Thank you for using our Setup Services.')
+    exit(0)
+
+# ****************************************************************
+#           GETTING INDIVIDUAL UNIQUE SECRET INFORMATION
+# ****************************************************************
+
+try:
+    with open('YOUR_BOT_API_TOKEN_HERE.txt', 'r') as fp:
+        bot_api_token = fp.read()
+        if bot_api_token.strip() == '':
+            raise ValueError('API TOKEN MISSING!')
+    # '1602420689:AAEeetAWtUatit1ubW1f821VdvJDqkQumBE'
+
+    with open('RECEIVER_ID_HERE.txt', 'r') as fp:
+        SEND_TO = fp.read()
+        if SEND_TO.strip() == '':
+            raise ValueError('RECEIVER ID MISSING!')
+    # arkadevans
+except (ValueError, FileNotFoundError) as e:
+    print(e)
+    print()
+    print("You Must Fill Up valid ['API TOKEN', 'API ID', 'API HASH', 'YOUR PHONE NO.', "
+          "'RECEIVER ID'] in the respective files before running this script.")
+    exit(0)
+
 # ****************************************************************
 #    TAKING SCREENSHOT AND SAVING INTO THE 'Screenshots' Folder
 # ****************************************************************
@@ -52,20 +80,12 @@ f = PATH + f_name
 ss.save(f)
 
 # ****************************************************************
-#    SETTING UP TELEGRAM CLIENT (FIRST TIME) AND SENDING IMAGE
+#                         SENDING IMAGE
 # ****************************************************************
 
-client = TelegramClient('session', api_id, api_hash)
-client.connect()
-
-if not client.is_user_authorized():
-    client.send_code_request(phone)
-    client.sign_in(phone,
-                   input('Enter the code sent to your phone: '))
-
 try:
-    client.send_file(SEND_TO, f)
-except Exception as e:
-    print(e)
-
-client.disconnect()
+    bot = telegram.Bot(token=bot_api_token)
+    bot.send_photo(SEND_TO, photo=open(f, 'rb'))
+    print("Screenshot send successfully!")
+except:
+    print("SENDING UNSUCCESSFUL!")
