1b9a234c9f985648e4bb611b17d80886fba70a3d
Arkadev Banerjee <arkadev220701@gmail.com>
2021-03-29 21:34:45 +0530

    Final Commit - Script Ready

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# Send-Screnshot-Telegram-Bot
+# Send Screenshots to Telegram Chat/Channel/Group using Bot
+
+## Instructions :-
+
+
+
+- **Step - 1 :** Create a virtual environment by running the following command using your terminal/cmd.
+
+**In Windows :**
+
+        `python -m venv venv`
+
+**In Mac/Linux :**
+        
+        `python3 -m venv venv`
+
+
+
+- **Step - 2 :** Activate the virtual environment by running the following command in your cmd/terminal after successfully setting up the virtual environment.
+
+**In Windows :**
+
+        `venv\Scripts\activate`
+
+**In Mac/Linux :**
+
+        `source venv/bin/activate`
+
+
+- **Step - 3 :** Install the necessary requirements for running the script - `main.py` by executing the following command there after
+
+**In Windows :**
+        
+        `pip install -r requirements.txt`
+
+**In Mac/Linux :**
+
+        `pip3 install -r requirements.txt`
+
+- **Step - 4 :** Run the main script by executing the following command in your terminal/cmd
+
+**In Windows :-**
+
+        `py main.py`
+
+**In Mac/Linux :-**
+
+        `python3 main.py`
+
+
+- **Step - 5 :** After running the script for the first time, three files will be created in the directory where the `main.py` script is placed. 
+
+    The File names are : 
+    - `YOUR_BOT_API_TOKEN_HERE.txt`,
+    - `RECEIVER_ID_HERE.txt`,
+    - `SCREENSHOT_DIR_DEFAULT_PATH_HERE.txt`
+
+    As the name suggests, enter your created Bot API TOKEN, Receiver Chat ID from telegram and the path of the directory where your screenshots are generally saved into respective files. If you do not know how to create API token, receiver ID or Bot in telegram, kindly google it.
+
+
+- **Step - 6 :** Congratulations! You have completed the setup and observer is implanted on your given path, screenshots will be automatically sent to the mentioned receiver whenever you take a screenshot. Stop the script when you're done and no screenshots will be uploaded thereafter. To start the observer again rerun the script :: `main.py`
+
--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import os
 import time
+import telegram
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-import telegram
+from datetime import datetime
+
+COUNTRY_CODE = '+91'
+PATH = ''
+
 
 # ****************************************************************
 #              CREATING SETUP FILES (FIRST TIME ONLY)
 # ****************************************************************
 
-curr_dir = os.getcwd()
-f_list = os.listdir(curr_dir)
+f_list = os.listdir(os.getcwd())
+
 
 inputs = [
     'YOUR_BOT_API_TOKEN_HERE.txt',
     'RECEIVER_ID_HERE.txt',
+    'SCREENSHOT_DIR_DEFAULT_PATH_HERE.txt'
 ]
 
 flag = 0
@@ -27,8 +33,6 @@ for it in f_list:
 
 if flag == 1:
     print('Setup Successful! Fill in your details in the files created and re-run the script again.')
-    print('To Set a Keybinding, Create a shortcut of the \'send.vbs\' file and set a keybinding there.')
-    print('Thank you for using our Setup Services.')
     exit(0)
 
 # ****************************************************************
@@ -40,13 +44,16 @@ try:
         bot_api_token = fp.read()
         if bot_api_token.strip() == '':
             raise ValueError('API TOKEN MISSING!')
-    # '1602420689:AAEeetAWtUatit1ubW1f821VdvJDqkQumBE'
 
     with open('RECEIVER_ID_HERE.txt', 'r') as fp:
         SEND_TO = fp.read()
         if SEND_TO.strip() == '':
             raise ValueError('RECEIVER ID MISSING!')
-    # arkadevans
+
+    with open('SCREENSHOT_DIR_DEFAULT_PATH_HERE.txt') as fp:
+    	PATH = fp.read()
+    	if PATH.strip() == '':
+    		raise ValueError('SCREENSHOT DIR DEFAULT PATH IS MISSING')
 except (ValueError, FileNotFoundError) as e:
     print(e)
     print()
@@ -60,30 +67,43 @@ except (ValueError, FileNotFoundError) as e:
 # ****************************************************************
 
 
-class ExampleHandler(FileSystemEventHandler):
-    count = 0
+class EventHandler(FileSystemEventHandler):
+    def __init__(self):
+        self.count = 0
+        self.first_screenshot_taken = False
 
     def on_created(self, event):
+        if not self.first_screenshot_taken:
+            print(f"Observer triggered : A new file detected : {event.src_path}")
+            print("The Script will not send the file using the telegram bot.")
+            print("Uploading started....")
         try:
             bot = telegram.Bot(token=bot_api_token)
             bot.send_photo(SEND_TO, photo=open(event.src_path, 'rb'))
             self.count += 1
-            print("Screenshot send successfully! - ", self.count)
+            print(f"Screenshot {self.count} : [Sent - {str(datetime.now().strftime('%d %B, %Y at %I:%M %p'))}]")
         except Exception as ev:
-            print("SENDING UNSUCCESSFUL!")
+            print("Error Occurred : SENDING UNSUCCESSFUL!")
             print(ev)
 
+        self.first_screenshot_taken = True
 
-observer = Observer()
-event_handler = ExampleHandler()
-observer.schedule(event_handler, path='C:\\Users\\arkad\\Pictures\\Screenshots')
-observer.start()
 
+# All the down four process is happening not in the main thread..
+# Daemon Thread
+
+observer = Observer() # Initiating the Observer Object
+event_handler = EventHandler() # Initiating the Event Handler Object
+observer.schedule(event_handler, path=PATH) # Implanting the observer at the given path
+observer.start() # Listening to Observer events....
+print(f"Observer Implanted for any creation changes at path {PATH}")
 
 try:
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
+    print('A KeyboardInterrupt was raised and the observer was stopped! Re-run the script to reinitialize the observer.')
     observer.stop()
 
 observer.join()
+print('Observer was stopped successfully!')
